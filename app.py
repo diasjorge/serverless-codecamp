53670b79a8a89509e2fb7e5ac6438e25595408a2
Jorge Dias <jorge.dias@schibsted.com>
2016-12-12 12:43:29 +0100

    Basic movements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from chalice import Chalice
 
 app = Chalice(app_name='serverless-codecamp')
-
+app.debug = True
 
 @app.route('/')
 def index():
@@ -18,27 +18,62 @@ def info():
 
 @app.route('/command', methods=['POST'], cors=True)
 def command():
+    map = app.current_request.json_body
+
+    print(map)
+
+    tank = Tank(map)
+    next_move = tank.next_move()
+
+    print("NEXT MOVE")
+    print(next_move)
+
     return {
-        'command': 'fire'
+        'command': next_move
     }
-# The view function above will return {"hello": "world"}
-# whenver you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.json_body
-#     # Suppose we had some 'db' object that we used to
-#     # read/write from our database.
-#     # user_id = db.create_user(user_as_json)
-#     return {'user_id': user_id}
-#
-# See the README documentation for more examples.
-#
+
+
+class Tank(object):
+
+    MOVEMENTS = {
+        'top':    {'x': 0, 'y': -1},
+        'left':   {'x': -1, 'y': 0},
+        'bottom': {'x': 0, 'y': 1},
+        'right':  {'x': 1, 'y': 0}
+    }
+
+    def __init__(self, map):
+        self._map = map
+
+    @property
+    def you(self):
+        return self._map['you']
+
+    @property
+    def next_position(self):
+        direction = self.you['direction']
+        next_movement = self.MOVEMENTS[direction]
+        return {
+            'x': self.you['x'] + next_movement['x'],
+            'y': self.you['y'] + next_movement['y']
+        }
+
+    def outside_of_map(self, position):
+        return position['x'] < 0 or position['x'] >= self._map['mapWidth'] or \
+            position['y'] < 0 or position['y'] >= self._map['mapHeight']
+
+    def wall_at(self, position):
+        walls = self._map['walls']
+        for wall in walls:
+            if wall['x'] == position['x'] and wall['y'] == position['y']:
+                return True
+
+        return False
+
+    def next_move(self):
+        if self.outside_of_map(self.next_position):
+            return 'turn-left'
+        elif self.wall_at(self.next_position):
+            return 'fire'
+        else:
+            return 'forward'
